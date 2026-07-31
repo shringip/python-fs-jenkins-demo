709d95fd14676088fb114c3c640e2b7134613d54
Prathmesh Shringi <prathmesh.s@tataelxsi.co.in>
2026-07-31 10:45:31 +0530

    added calculator tests

--- app/calculator.py
+++ app/calculator.py
@@ -4,6 +4,16 @@ def add(a,b):
 def substract(a,b):
     return a-b
 
+def multiply(a,b):
+    return a*b
+
+def divison(a,b):
+    if b==0:
+        raise ValueError("cannot divide by 0")
+    return a/b
+
 if __name__=="__main__":
     print("add result is",add(23,4))
     print("substract result is",substract(34,12))
+    print("multiply result is",multiply(3,9))
+    print("division result is",divison(60,12))
---
+++ tests/test_calculator.py
@@ -0,0 +1,22 @@
+import pytest
+ 
+from app.calculator import add,substract,multiply,division
+ 
+ 
+def test_add():
+    assert add(10,20)==40
+ 
+def test_subtract():
+    assert substract(20,10)==10
+ 
+def test_multiply():
+    assert multiply(4,4)==16
+ 
+def test_division():
+    assert division(20,5)==4
+ 
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        division(10,0)
+ 
+ 
