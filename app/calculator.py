cedf3e4f0ca4e3a1f3773d8f80027e9cfb16e90f
Prathmesh Shringi <prathmesh.s@tataelxsi.co.in>
2026-07-31 10:08:55 +0530

    sample app created

---
+++ app/calculator.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
+
+if __name__=="__main__":
+    print("add result is",add(23,4))
+    print("substract result is",substract(34,12))
