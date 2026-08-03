393f024bb932b3f24eaee23b39150f38b7895e7b
Prathmesh Shringi <prathmesh.s@tataelxsi.co.in>
2026-08-03 11:05:38 +0530

    minor changes in add function

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,7 +4,7 @@ from app.calculator import add,substract,multiply,divison
  
 @pytest.mark.smoke
 def test_add():
-    assert add(10,20)==30
+    assert add(1,20)==21
  
 def test_subtract():
     assert substract(20,10)==10
